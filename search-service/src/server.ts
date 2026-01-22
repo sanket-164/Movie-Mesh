c37ba93c31be16c699d0b77d12969c89312522c0
Sanket Sadadiya <sanketsadadiya53@gmail.com>
2026-01-22 11:16:45 +0530

    Search microservice setup

---
+++ search-service/src/server.ts
@@ -0,0 +1,39 @@
+import express from 'express';
+import cors from 'cors';
+
+class SearchServer {
+    private app: express.Express;
+    private port: number;
+
+    constructor(port: number) {
+        this.app = express();
+        this.port = port;
+
+        this.initializeMiddlewares();
+        this.initializeRoutes();
+    }
+
+    private initializeMiddlewares(): void {
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
+        this.app.use(cors());
+    }
+
+    private initializeRoutes(): void {
+        this.app.get('/', (req, res) => {
+            res.send('Search Server is running');
+        });
+        this.app.use((req, res) => {
+            res.status(404).send('Route not found');
+        });
+    }
+
+    public listen(): void {
+        this.app.listen(this.port, () => {
+            console.log(`Search server is listening at http://localhost:${this.port}`);
+        });
+    }
+}
+
+const server = new SearchServer(process.env.PORT ? parseInt(process.env.PORT) : 3000);
+server.listen();
