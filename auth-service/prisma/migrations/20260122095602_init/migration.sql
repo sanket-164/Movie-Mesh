8d5c73ea2cef5ab3421b9db2eb315b8e01045d2b
Sanket Sadadiya <sanketsadadiya53@gmail.com>
2026-01-22 15:44:52 +0530

    Changed MySQL schema

---
+++ auth-service/prisma/migrations/20260122095602_init/migration.sql
@@ -0,0 +1,15 @@
+-- AlterTable
+ALTER TABLE `AuthLog` MODIFY `action` ENUM('LOGIN', 'LOGOUT', 'PASSWORD_RESET', 'DELETE_ACCOUNT') NOT NULL;
+
+-- CreateTable
+CREATE TABLE `SearchLog` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `query` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `timestamp` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `SearchLog` ADD CONSTRAINT `SearchLog_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
