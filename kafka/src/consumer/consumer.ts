342e5dcc4f44f91a9c9fc5980d876360f2a213a8
Sanket Sadadiya <sanketsadadiya53@gmail.com>
2026-01-28 16:37:08 +0530

    Fix kafka consumer connection bug

--- kafka/src/consumer/consumer.ts
+++ kafka/src/consumer/consumer.ts
@@ -11,9 +11,19 @@ abstract class KafkaConsumer {
     }
 
     async start(): Promise<void> {
-        await this.consumer.connect();
-
-        await this.consumer.subscribe({ topic: this.topic });
+        // Consumer startup
+        const maxRetries = 5;
+        for (let i = 0; i < maxRetries; i++) {
+            try {
+                await this.consumer.connect();
+                await this.consumer.subscribe({ topic: this.topic });
+                break; // Success!
+            } catch (e) {
+                if (i === maxRetries - 1) throw e;
+                console.log(`Retry ${i + 1}/${maxRetries} after topic error...`);
+                await new Promise(res => setTimeout(res, 2000 * (i + 1)));
+            }
+        }
 
         await this.consumer.run({
             eachMessage: async (payload: EachMessagePayload) => {
